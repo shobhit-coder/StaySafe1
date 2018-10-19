587d578684a7bf4ac1079640cfefbcd4658e8592
shobhit-coder <shobhit1998@yahoo.co.in>
2018-10-20 00:02:57 +0530

    added stuff

--- app/src/main/java/com/example/android/staysafe1/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/android/staysafe1/MyFirebaseMessagingService.java
@@ -1,5 +1,7 @@
 package com.example.android.staysafe1;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
@@ -19,21 +21,37 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d("Message Received", "From: " + remoteMessage.getFrom());
+        Intent intent=new Intent(this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("message",remoteMessage.getData().toString());
+        PendingIntent pendingIntent=PendingIntent.getActivity(this,0/*request code*/,intent,PendingIntent.FLAG_ONE_SHOT);
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d("datapayload", "Message data payload: " + remoteMessage.getData());
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
                     .setSmallIcon(R.drawable.small)
-                    .setContentTitle("Jhaatu Disaster Alert")
-                    .setContentText("Magnitude: xxx  \nLocation: xxx"+remoteMessage.getData())
+                    .setContentTitle("Alerts Received")
+                    .setContentText("Tap here to look at the updated map.")
                     .setStyle(new NotificationCompat.BigTextStyle()
-                            .bigText("ooooooooooooooooooMagnitude: xxx \nLocation: xxx"+remoteMessage.getData()))
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                            .bigText("Tap here to look at the updated map."))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent);
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
             // notificationId is a unique int for each notification that you must define
             notificationManager.notify(1, mBuilder.build());
+
+//            NotificationCompat.Builder notifiBuilder=new NotificationCompat.Builder(this)
+//                    .setSmallIcon(R.mipmap.ic_launcher)
+//                    .setContentTitle("Alerts Received")
+//                    .setContentText("Tap here to look at the updated map.")
+//                    .setAutoCancel(true)
+//                    .setContentIntent(pendingIntent);
+//            NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+//            notificationManager.notify(0/*ID of notification*/,notifiBuilder.build());
+
+
 
             if (/* Check if data needs to be processed by long running job */ true) {
                 // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
